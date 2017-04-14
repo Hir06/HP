9ec89cd7430fe91699f553a28b63ccbcc8090c49
hir06 <phiral246@gmail.com>
2017-04-14 18:49:05 +0530

    Folder structure revisited

---
+++ blog/ng-app/components/work/work.module.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    "use strict";
+
+    angular.module('myApp.work', [
+        "myApp.work.controller",
+        "myApp.work.services",
+    ])
+    .config(routeConfig);
+
+    routeConfig.$inject = ['$routeProvider'];
+
+    function routeConfig($routeProvider) {
+        $routeProvider.when('/dashBoard', {
+            controller: 'workController',
+            controllerAs: 'DashboardVM',
+            templateUrl: 'ng_app/components/work/work.html',
+            //resolve: {}
+        });
+    }
+
+})();
---
+++ blog/ng-app/myApp.js
@@ -0,0 +1,24 @@
+﻿
+
+(function () {
+    'use strict';
+    var myApp = angular.module("myApp", [
+                    'ngRoute',
+                    'myApp.work'
+                  
+    ])
+
+
+    myApp.config(['$routeProvider', '$compileProvider', '$locationProvider',
+                      function ($routeProvider, $compileProvider, $locationProvider) {
+                          $routeProvider
+                            .otherwise({
+                                redirectTo: '/DashBoard'
+                            });
+                      }]);
+
+    myApp.run(function ($rootScope, $window) {
+        console.log("myApp started successfully!");
+    });
+
+})();
