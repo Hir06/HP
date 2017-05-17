58415b2589baa2541ac576876d28c0a06e486025
Hir06 <phiral246@gmail.com>
2017-05-17 12:15:39 +0530

    Deleted Images

--- blog/ng-app/components/workDetail/workDetail.controller.js
+++ blog/ng-app/components/workDetail/workDetail.controller.js
@@ -16,19 +16,20 @@
    
         _this.fetchProjectDetails = fetchProjectDetails;
         _this.showRightImage = showRightImage;
-      
+        _this.imagError = imagError;
+        _this.showLeftImage = showLeftImage;
         function fetchProjectDetails() {
             var promiseObj = myAppService.fetchProjectDetails();
             promiseObj.then(function success(data) {
                 
                     _this.projectData = data.projectData;
-                    _this.imageCount = [];
-                    for (var i = 0; i < 5; i++) {
-                        _this.imageCount.push({index:i+1})
-                    }
+                    //_this.imageCount = [];
+                    //for (var i = 0; i < 5; i++) {
+                    //    _this.imageCount.push({index:i+1})
+                    //}
 
                     _this.projectName = $routeParams.projectName;
-                    _this.projectName.replace('', '%20');
+                    //_this.projectName.replace('', '%20');
             },
             function error() {
                 Materialize.toast("Couldn't load projectData!", 4000, "red")
@@ -40,9 +41,29 @@
             var imageIndex = object.target.previousElementSibling.src.split("/").pop(-1).split('.')[0];
             imageIndex++;
             var imageUrl = object.target.previousElementSibling.src;
-            $('#workImage').attr('src', 'imageUrl');
+            var temp = imageUrl.split(".")[0].replace(/\d+$/, imageIndex) + ".png";
+            //imageUrl = temp.replace(/\d+$/, imageIndex++);
+            //imageUrl=imageUrl+ ".png"
+            $('#workImage').attr('src', temp);
            
 
+        }
+        function getFoldersize() {
+
+        }
+        function showLeftImage(object) {
+            var imageIndex = object.target.previousElementSibling.src.split("/").pop(-1).split('.')[0];
+            imageIndex++;
+            var imageUrl = object.target.previousElementSibling.src;
+            var temp = imageUrl.split(".")[0].replace(/\d+$/, imageIndex) + ".png";
+            //imageUrl = temp.replace(/\d+$/, imageIndex++);
+            //imageUrl=imageUrl+ ".png"
+            $('#workImage').attr('src', temp);
+
+
+        }
+        function imagError(object) {
+            $('#workImage').attr('src', 'src');
         }
 
 
