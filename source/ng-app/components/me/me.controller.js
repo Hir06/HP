029dd0892deee2004e2ca2dab17e42269d5675b5
Hir06 <phiral246@gmail.com>
2017-08-08 19:22:25 +0530

    Footer modified

---
+++ source/ng-app/components/me/me.controller.js
@@ -0,0 +1,22 @@
+﻿(function() {
+    "use strict";
+    angular.module('myApp.me.controller', [])
+        .controller('meController', meController);
+
+    meController.$inject = ['$timeout', '$scope', 'meService', 'appConstants'];
+
+    function meController($timeout, $scope, meService, appConstants) {
+
+
+        var _this = this;
+        _this.designer = false;
+        $('.parallax').parallax();
+
+        // _this.changeGray = changeGray;
+
+        // function changeGray() {
+        //     $('.designer-img').css('background-position', '0px');
+        //     _this.designer = true;
+        // }
+    }
+})();
